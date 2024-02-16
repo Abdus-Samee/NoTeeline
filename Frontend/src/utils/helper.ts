bb64465423f60ce9fe696f4be5e20938c5f71df0
Abdus Samee <abdussamee16@gmail.com>
2024-02-16 18:41:10 +0600

    Update: Added new openai key

--- Frontend/src/utils/helper.ts
+++ Frontend/src/utils/helper.ts
@@ -4,7 +4,7 @@ import { NotePoint, TranscriptLine, OnboardingSection } from "../state/noteStore
 // const SEED = 1
 const WINDOW_SIZE = 20000 //20000ms
 // const OPEN_AI_KEY = import.meta.env.VITE_OPEN_AI_KEY
-const OPEN_AI_KEY = "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"
+const OPEN_AI_KEY = "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"
 export const openai = new OpenAI({ apiKey: OPEN_AI_KEY, dangerouslyAllowBrowser: true })
 // const template = "You are a note taking assistant. Users will give you their summary and the meeting transcript."+
 //                  "You have to expand it to 2-3 full sentences in simple english.\nHere is one example:\n"+
@@ -117,7 +117,7 @@ export const callGPT = async (points: {point: string, history: string[], expand:
 
             const res = await openai.chat.completions.create({
                 messages: [{ role: "system", content: PROMPT }],
-                model: "gpt-3.5-turbo",
+                model: "gpt-4-1106-preview",
             })
 
             if(res?.choices[0]?.message?.content !== null) expansion.push({point: point.point, expansion: res.choices[0].message.content, old: false})
