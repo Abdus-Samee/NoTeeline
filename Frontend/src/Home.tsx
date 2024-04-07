7da1e9ed0eeef30ba7e04e7ad0bbcfac366a11dd
Abdus Samee <abdussamee16@gmail.com>
2024-04-07 13:26:58 +0600

    Update: Home Page

--- Frontend/src/Home.tsx
+++ Frontend/src/Home.tsx
@@ -7,9 +7,15 @@ import {
   FormHelperText,
   Button,
   Input,
+  Flex,
+  Spacer,
+  Box,
+  Heading,
   Text,
   useToast,
 } from '@chakra-ui/react'
+import { InfoOutlineIcon } from '@chakra-ui/icons'
+import { Typewriter } from 'react-simple-typewriter'
 
 const Home = () => {
   const [gptKey, setGptKey] = useState<string>('')
@@ -18,7 +24,8 @@ const Home = () => {
   const toast = useToast()
   const navigate = useNavigate()
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
     console.log(gptKey)
     if(gptKey === ''){
       setErrorMsg('No GPT key provided...')
@@ -30,41 +37,68 @@ const Home = () => {
   }
   
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: '15vh',
-    }}>
-    <Text fontSize='5xl' style={{ color: 'purple', }}>NoTeeline</Text>
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: '20%',
-      marginTop: '8vh',
-      borderRadius: '10px',
-      backgroundColor: '#BCA37F',
-      minWidth: '25vw',
-    }}>
-      <FormControl>
-        <Input 
-          type='text' 
-          value={gptKey} 
-          placeholder='Enter your GPT Key'
-          _placeholder={{ color: 'grey.500', }}
-          style={{ color: '#fff', border: '1px solid #000', background: '#54432C', }}
-          onChange={(e) => setGptKey(e.target.value)}
+    <Flex
+      width='full'
+      flexDirection='column'
+      align='center'
+      justify='center'
+      style={{ marginTop: '20vh', }}
+    >
+      <Box mb={10} style={{ fontWeight: '600', fontSize: '18px', }}>
+        <Typewriter
+          words={['Write, Organize, Review, and Summarize Personalized Notes']}
+          cursor
+          cursorStyle='_'
+          typeSpeed={30}
         />
-        {errorMsg !== '' && 
-          <FormHelperText style={{ color: 'red', }}>{errorMsg}</FormHelperText>
-        }
-        <Button colorScheme='green' style={{ marginTop: '1vh', }} onClick={handleSubmit}>Submit</Button>
-      </FormControl>
-    </div>
-    </div>
-  )
+      </Box>
+      <Box
+        p={8}
+        minWidth='25vw'
+        maxWidth='30vw'
+        borderWidth={1}
+        borderRadius={8}
+        boxShadow='lg'
+      >
+        <Box textAlign='center'>
+          <Heading color='#4338CA'>NoTeeline</Heading>
+        </Box>
+        <Box my={4} textAlign='center'>
+          <form>
+            <FormControl>
+              <Input 
+                type='text'
+                placeholder='Enter your GPT4 key'
+                onChange={(e) => setGptKey(e.target.value)}
+              />
+              {errorMsg !== '' &&
+                <FormHelperText
+                  style={{ color: 'red', fontWeight: 'bold', }}
+                >
+                  {errorMsg}
+                </FormHelperText>
+              }
+            </FormControl>
+            <Button
+              width='half'
+              type='submit'
+              mt={4}
+              colorScheme='teal'
+              variant='outline'
+              onClick={handleSubmit}
+            >
+              Submit
+            </Button>
+          </form>
+        </Box>
+        <Box textAlign='left'>
+          <Text fontSize='xs' color='grey' as='em'>
+            <InfoOutlineIcon /> The GPT4 key you provide will be used for the expansion of micronotes, theme generation, cue questions, and summarization. Be careful to provide the correct key!
+          </Text>
+        </Box>
+      </Box>
+    </Flex>
+   )
 }
 
 export default Home
