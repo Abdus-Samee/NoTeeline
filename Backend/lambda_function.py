fc96d4409ca4660ed6dfd723ab9870740d83b499
Abdus Samee <53638835+Abdus-Samee@users.noreply.github.com>
2024-09-30 20:51:06 +0000

    Update: Fixed proxy with certificate

--- Backend/lambda_function.py
+++ Backend/lambda_function.py
@@ -1,10 +1,5 @@
-'''
-    Used in the AWS Lambda
-    to fetch YouTube transcript
-    using multiple proxies
-'''
-
 import json
+import requests
 import logging
 from youtube_transcript_api import YouTubeTranscriptApi
 
@@ -12,19 +7,27 @@ from youtube_transcript_api import YouTubeTranscriptApi
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+# Monkey-patch the requests.Session.request method
+def patched_request(self, *args, **kwargs):
+    kwargs['verify'] = 'zyte-ca.crt'  # Path to your custom CA certificate
+    return original_request(self, *args, **kwargs)
+
+original_request = requests.Session.request
+requests.Session.request = patched_request
+
 def lambda_handler(event, context):
     
     # Get the length and width parameters from the event object. The 
     # runtime converts the event object to a Python dictionary
     vid = event['queryStringParameters']['video_id']
-    # vid = event['video_id'] # For testing inside AWS console
+    # vid = event['video_id']
 
     tr = get_transcript(vid)
     print(f"The transcript is {tr}")
         
     logger.info(f"CloudWatch logs group: {context.log_group_name}")
     
-    # return the transcript as a JSON string
+    # return the calculated area as a JSON string
     data = {"transcript": tr}
     return json.dumps(data)
     
@@ -34,10 +37,12 @@ def get_transcript(video_id):
     # proxy = 'http://coydbahd:p01esrad3gzh@204.44.69.89:6342'
     # proxy = 'http://coydbahd:p01esrad3gzh@64.64.118.149:6732'
     proxies = [
-        'http://coydbahd:p01esrad3gzh@173.0.9.209:5792',
-        'http://vqmpgykm:nl7enetumge3@204.44.69.89:6342',
-        'http://coydbahd:p01esrad3gzh@204.44.69.89:6342',
-        'http://coydbahd:p01esrad3gzh@64.64.118.149:6732'
+        'http://44b60d31540c4fa79dd3f13918342d01:@api.zyte.com:8011/'
+        # 'http://rcelkpui:he7echtlinf9@104.239.105.125:6655',
+        # 'http://coydbahd:p01esrad3gzh@173.0.9.209:5792',
+        # 'http://vqmpgykm:nl7enetumge3@204.44.69.89:6342',
+        # 'http://coydbahd:p01esrad3gzh@204.44.69.89:6342',
+        # 'http://coydbahd:p01esrad3gzh@206.41.172.74:6634',
     ]
     
     result = ''
--- Frontend/src/utils/loadDemoNote.ts
+++ Frontend/src/utils/loadDemoNote.ts
@@ -5,8 +5,200 @@ export const getDemoNote = (): Note_t => {
         name: "Demo Note",
         ytId: "w0QbnxKRD0w",
         micronote: true,
-        content: [],
-        transcription: [],
+        content: [
+            {
+                "created_at": 7.265976003814697,
+                "point": "tapping int conn",
+                "utc_time": 1727725336596
+            },
+            {
+                "created_at": 15.339126043869019,
+                "point": "overhearin pckts",
+                "utc_time": 1727725344733
+            },
+            {
+                "created_at": 22.419582003814696,
+                "point": "eaves dropping",
+                "utc_time": 1727725352512
+            },
+            {
+                "created_at": 34.53803586648559,
+                "point": "wiretap connection",
+                "utc_time": 1727725363887
+            },
+            {
+                "created_at": 43.619564064849854,
+                "point": "https",
+                "utc_time": 1727725372948
+            }
+        ],
+        transcription: [
+            {
+                "text": "Can your internet connection be tapped like\na telephone line?",
+                "offset": 2.12,
+                "duration": 3.54
+            },
+            {
+                "text": "In a word, Yes!",
+                "offset": 5.66,
+                "duration": 1.11
+            },
+            {
+                "text": "If you’re using a wireless network to Google\nsomething,",
+                "offset": 6.77,
+                "duration": 3.23
+            },
+            {
+                "text": "other devices in the room with wireless connectivity\ncan overhear your packets (byte-sized-message)",
+                "offset": 10,
+                "duration": 4.41
+            },
+            {
+                "text": "and see what you’re doing!",
+                "offset": 14.41,
+                "duration": 2.32
+            },
+            {
+                "text": "Overhearing a conversation like conversation\nin real life (in the cyber world) is also",
+                "offset": 16.73,
+                "duration": 4.299
+            },
+            {
+                "text": "called as Eavesdropping!",
+                "offset": 21.029,
+                "duration": 1.791
+            },
+            {
+                "text": "Your neighbor or a person sitting outside\nyour house can eavesdrop with the right hardware",
+                "offset": 22.82,
+                "duration": 4.09
+            },
+            {
+                "text": "and software.",
+                "offset": 26.91,
+                "duration": 1.61
+            },
+            {
+                "text": "If you’re using a wired connection,\none can directly wiretap your connection just",
+                "offset": 28.52,
+                "duration": 4.47
+            },
+            {
+                "text": "like a telephone line.",
+                "offset": 32.99,
+                "duration": 1.7
+            },
+            {
+                "text": "If you want your communication to be private,\nencryption is the key!",
+                "offset": 34.69,
+                "duration": 4.169
+            },
+            {
+                "text": "The HTTPS in web-browser indicates that your\ncommunication with the server is encrypted.",
+                "offset": 38.859,
+                "duration": 5.72
+            },
+            {
+                "text": "Everyone can still eavesdrop but they can’t\nunderstand the content of the conversation!",
+                "offset": 44.579,
+                "duration": 5.281
+            },
+            {
+                "text": "But how HTTPS works?",
+                "offset": 49.86,
+                "duration": 1.92
+            },
+            {
+                "text": "Let’s say you lock some confidential information\nin a box using a unique symmetric key and",
+                "offset": 51.78,
+                "duration": 6.18
+            },
+            {
+                "text": "send it (the key and the box) to the server.",
+                "offset": 57.96,
+                "duration": 1.93
+            },
+            {
+                "text": "The server opens the box using the key it\nreceived along with the box.",
+                "offset": 59.89,
+                "duration": 3.93
+            },
+            {
+                "text": "But anyone tapping this connection also receives\nthe box and the key.",
+                "offset": 63.82,
+                "duration": 4.93
+            },
+            {
+                "text": "And this is a problem!",
+                "offset": 68.75,
+                "duration": 2.04
+            },
+            {
+                "text": "To solve this, we’ve a special type of box\nat the server side which can be locked using",
+                "offset": 70.79,
+                "duration": 4.47
+            },
+            {
+                "text": "a public key but opened only using a private\nkey.",
+                "offset": 75.26,
+                "duration": 4.4
+            },
+            {
+                "text": "The server provides you this special box and\npublic key but keeps the private key secret",
+                "offset": 79.66,
+                "duration": 4.4
+            },
+            {
+                "text": "from everyone.",
+                "offset": 84.06,
+                "duration": 1
+            },
+            {
+                "text": "You can put your original box and unique symmetric\nkey inside this special box,",
+                "offset": 85.06,
+                "duration": 5.73
+            },
+            {
+                "text": "lock it using server’s public key and send\nit to the server.",
+                "offset": 90.79,
+                "duration": 4.43
+            },
+            {
+                "text": "Only the server can open this special box\nusing its private key.",
+                "offset": 95.22,
+                "duration": 4.27
+            },
+            {
+                "text": " ",
+                "offset": 99.49,
+                "duration": 1
+            },
+            {
+                "text": "Therefore you’ve safely shared the unique\nsymmetric key which can be used to unlock",
+                "offset": 100.49,
+                "duration": 3.22
+            },
+            {
+                "text": "and lock the original box.",
+                "offset": 103.71,
+                "duration": 2.12
+            },
+            {
+                "text": "Now your browser and server can exchange data\nin normal boxes and need not share the unique",
+                "offset": 105.83,
+                "duration": 4.88
+            },
+            {
+                "text": "symmetric key again.",
+                "offset": 110.71,
+                "duration": 2.21
+            },
+            {
+                "text": "This is how encryption works, except there\nis no box!",
+                "offset": 112.92,
+                "duration": 18.18
+            }
+        ],
         expansion: [],
         generatedSummary: "",
         generatedSummary_P: "",
@@ -15,7 +207,7 @@ export const getDemoNote = (): Note_t => {
         expand_count: 0,
         created_at: Date.now(),
         updated_at: Date.now(),
-        recording_start: 0
+        recording_start: Date.now()
     }
 
     return demo_note
